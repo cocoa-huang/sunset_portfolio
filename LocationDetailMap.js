7f1545f0f8447451c6adc5e0280e5e1beae5ca8f
Eric Huang <63391548+cocoa-huang@users.noreply.github.com>
2025-05-30 18:16:04 -0700

    Add files via upload

---
+++ LocationDetailMap.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix for Leaflet icon issues in Next.js
+// This is necessary because of how Next.js handles assets
+const DefaultIcon = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+// Custom sunset marker icon with an orange color
+const SunsetIcon = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+export default function LocationDetailMap({ location }) {
+  if (!location) return null;
+  
+  const position = [location.coordinates.lat, location.coordinates.lng];
+  
+  return (
+    <MapContainer 
+      center={position} 
+      zoom={10} 
+      style={{ height: '100%', width: '100%' }}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={SunsetIcon}>
+        <Popup>
+          <div>
+            <strong>{location.name}</strong><br />
+            {location.viewpoint}
+          </div>
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+} 
